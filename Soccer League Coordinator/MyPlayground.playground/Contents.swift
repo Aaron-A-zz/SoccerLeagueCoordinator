cf81cbe095ec2cd966bce5cbb4f85be40126d480
Aaron <Aaron@Mav3r1ck.io>
2016-04-10 21:13:57 -0400

    removed class added dictionary

--- Soccer League Coordinator/MyPlayground.playground/Contents.swift
+++ Soccer League Coordinator/MyPlayground.playground/Contents.swift
@@ -1,160 +1,93 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+import Foundation
+import GameKit
 
-//Enum for Rookie vs Pro status
-enum Experience {
-    case Rookie
-    case Pro
+var joeSmith = ["name": "Joe Smith", "height": 42, "experience": true, "guardin": "Jim & Jan Smith"]
+var jillTanner = ["name": "Jill Tanner", "height": 36, "experience": true, "guardin": "Clara Tanner"]
+var billBon = ["name": "Bill Bon", "height": 43, "experience": true, "guardin": "Sara & Jenny Bon"]
+var evaGordon = ["name": "Eva Gordon", "height": 43, "experience": false, "guardin": "Wendy and Mike Gordon"]
+var mattGill = ["name": "Matt Gill", "height": 40, "experience": false, "guardin": "Charles & Sylvia Gill"]
+var kimmyStein = ["name": "Kimmy Stein", "height": 41, "experience": false, "guardin": "Bill & Hillary Stein"]
+var sammyAdams = ["name": "Sammy Adams", "height": 45, "experience": false, "guardin": "Jeff Adams"]
+var karlSaygan = ["name": "Karl Saygan", "height": 42, "experience": true, "guardin": "Heather Bledsoe"]
+var suzaneGreenberg = ["name": "Suzane Greenberg", "height": 44, "experience": true, "guardin": "Wendy and Mike Gordon"]
+var salDali = ["name": "Sal Dali", "height": 41, "experience": false, "guardin": "Wendy and Mike Gordon"]
+var joeKavalier = ["name": "Joe Kavalier", "height": 39, "experience": false, "guardin": "Wendy and Mike Gordon"]
+var benFinkelstein = ["name": "Ben Finkelstein", "height": 44, "experience": false, "guardin": "Wendy and Mike Gordon"]
+var diegoSoto = ["name": "Diego Soto", "height": 41, "experience": true, "guardin": "Wendy and Mike Gordon"]
+var chloeAlaska = ["name": "Chloe Alaska", "height": 47, "experience": false, "guardin": "Wendy and Mike Gordon"]
+var arnoldWillis = ["name": "Arnold Willis", "height": 43, "experience": false, "guardin": "Wendy and Mike Gordon"]
+var phillipHelm = ["name": "Phillip Helm", "height": 44, "experience": true, "guardin": "Wendy and Mike Gordon"]
+var lesClay = ["name": "Les Clay", "height": 42, "experience": true, "guardin": "Wendy and Mike Gordon"]
+var herschelKrustofski = ["name": "Herschel Krustofski", "height": 45, "experience": true, "guardin": "Wendy and Mike Gordon"]
+
+var soccerLeague = [joeSmith, jillTanner, billBon, evaGordon,mattGill,kimmyStein,sammyAdams,karlSaygan,suzaneGreenberg,salDali,joeKavalier,benFinkelstein,diegoSoto,chloeAlaska,arnoldWillis,phillipHelm,lesClay,herschelKrustofski]
+
+var dragons = [[String:AnyObject]]()
+var sharks = [[String:AnyObject]]()
+var raptors = [[String:AnyObject]]()
+
+var teams = [dragons,sharks,raptors]
+var rookieList = [[String:AnyObject]]()
+var proList = [[String:AnyObject]]()
+var numberOfPlayersPerTeam = soccerLeague.count / teams.count
+
+func creatTeams() {
+    
+    for player in soccerLeague {
+        
+        //sort rookies & pro players
+        if player["experience"] == 0 {
+            rookieList.append(player)
+        } else {
+            proList.append(player)
+        }
+    }
+    
+    for player in rookieList {
+        
+        if dragons.count < numberOfPlayersPerTeam / 2 {
+            dragons.append(player)
+        } else if sharks.count < numberOfPlayersPerTeam / 2  {
+            sharks.append(player)
+        } else if raptors.count < numberOfPlayersPerTeam / 2  {
+            raptors.append(player)
+        }
+    }
+    
+    for player in proList {
+        
+        if dragons.count < numberOfPlayersPerTeam  {
+            dragons.append(player)
+        } else if sharks.count < numberOfPlayersPerTeam  {
+            sharks.append(player)
+        } else if raptors.count < numberOfPlayersPerTeam  {
+            raptors.append(player)
+        }
+    }
+    
+    
 }
 
-//Player class to store players info
-class Player {
-    
-    let name: String
-    let height: Int
-    let guardianName: String
-    let experience: Experience
-    var discription: String {
-        return "name: \(name) height: \(height) guardian:\(guardianName)"
-    }
-    
-    init(name: String, height: Int, experience: Experience, guardinaName: String) {
-        
-        self.name = name
-        self.height = height
-        self.experience = experience
-        self.guardianName = guardinaName
-    }
-}
-
-
-// List of all players:
-var JoeSmith = Player(name: "Joe Smith", height: 42, experience: .Pro, guardinaName: "Jim & Jan Smith")
-var jillTanner = Player(name: "Jill Tanner", height: 36, experience: .Pro, guardinaName: "Clara Tanner")
-var billBon = Player(name: "Bill Bon", height: 43, experience: .Pro, guardinaName: "Sara & Jenny Bon")
-var evaGordon = Player(name: "Eva Gordon", height: 43, experience: .Rookie, guardinaName: "Wendy and Mike Gordon")
-var mattGill = Player(name: "Matt Gill", height: 40, experience: .Rookie, guardinaName: "Charles & Sylvia Gill")
-var kimmyStein = Player(name: "Kimmy Stein", height: 41, experience: .Rookie, guardinaName: "Bill & Hillary Stein")
-var sammyAdams = Player(name: "Sammy Adams", height: 45, experience: .Rookie, guardinaName: "Jeff Adams")
-var karlSaygan = Player(name: "Karl Saygan", height: 42, experience: .Pro, guardinaName: "Heather Bledsoe")
-var suzaneGreenberg = Player(name: "Suzane Greenberg", height: 44, experience: .Pro, guardinaName: "Henrietta Dumas")
-var salDali = Player(name: "Sal Dali", height: 41, experience: .Rookie, guardinaName: "Gala Dali")
-var joeKavalier = Player(name: "Joe Kavalier", height: 39, experience: .Rookie, guardinaName: "Sam & Elaine Kavalier")
-var benFinkelstein = Player(name: "Ben Finkelstein", height: 44, experience: .Rookie, guardinaName: "Aaron & Jill Finkelstein")
-var diegoSoto = Player(name: "Diego Soto", height: 41, experience: .Pro, guardinaName: "Robin & Sarika Soto")
-var chloeAlaska = Player(name: "Chloe Alaska", height: 47, experience: .Rookie, guardinaName: "David & Jamie Alaska")
-var arnoldWillis = Player(name: "Arnold Willis", height: 43, experience: .Rookie, guardinaName: "Claire Willis")
-var phillipHelm = Player(name: "Phillip Helm", height: 44, experience: .Pro, guardinaName: "Thomas Helm & Eva Jones")
-var lesClay = Player(name: "Les Clay", height: 42, experience: .Pro, guardinaName: "Wynonna Brown")
-var herschelKrustofski = Player(name: "Herschel Krustofski", height: 45, experience: .Pro, guardinaName: "Hyman & Rachel Krustofski")
-
-
-//  Creating an Array of Players Class
-var soccerLeague = [JoeSmith, jillTanner, billBon, evaGordon,mattGill,kimmyStein,sammyAdams,karlSaygan,suzaneGreenberg,salDali,joeKavalier,benFinkelstein,diegoSoto,chloeAlaska,arnoldWillis,phillipHelm,lesClay,herschelKrustofski]
-
-
-var dragons: [Player] = []
-var sharks: [Player] = []
-var raptors: [Player] = []
-var playersAlreadyAdded: [Player] = []
-var proPlayers = [Player]()
-var rookiePlayers = [Player]()
-
-
-
-//Logic
-func createTeams(team: [Player]) -> ([Player],[Player],[Player]) {
-    
-    // Step one: sort the league Pros from the scrubs.
-    // A  better way of sorting the league would be...let proPlayers = soccerLeague.filter { $0.experience == .Pro } you could also do this for the rookie players if you wanted. 
-    
-    for players in soccerLeague {
-        if players.experience == .Pro {
-            proPlayers.append(players)
-        } else if players.experience == .Rookie {
-            rookiePlayers.append(players)
-        }
-    }
-    
-    proPlayers.count
-    rookiePlayers.count
-    
-    // Step 3: Add three pro players to each team
-    //Before I add a player I check to see if they have already been added to another team.
-    for players in proPlayers {
-        var playerIsNew = true
-        
-        for player in playersAlreadyAdded {
-            if players.name == player.name {
-                playerIsNew = false
-                break
-            }
-        }
-        
-        if playerIsNew == true {
-            if dragons.count < 3 {
-                playersAlreadyAdded.append(players)
-                dragons.append(players)
-                
-            } else if sharks.count < 3 {
-                playersAlreadyAdded.append(players)
-                sharks.append(players)
-                
-            } else if raptors.count < 3 {
-                playersAlreadyAdded.append(players)
-                raptors.append(players)
-            }
-        }
-    }
-    
-    
-    //Step 4: Add three rookie players to each team
-    //Before I add a player I check to see if they have already been added to another team.
-    for players in rookiePlayers {
-        var playerIsNew = true
-        
-        for player in playersAlreadyAdded {
-            if players.name == player.name {
-                playerIsNew = false
-                break
-            }
-        }
-        
-        if playerIsNew == true {
-            if dragons.count < 6 {
-                playersAlreadyAdded.append(players)
-                dragons.append(players)
-                
-            } else if sharks.count < 6 {
-                playersAlreadyAdded.append(players)
-                sharks.append(players)
-                
-            } else if raptors.count < 6 {
-                playersAlreadyAdded.append(players)
-                raptors.append(players)
-            }
-        }
-    }
-    
-    //Step 5: Print out the welcome letters!
-    print("******DRAGONS*******")
+func printTeamLetters() {
+    print("*****Team Dragons*****")
     for players in dragons {
-        print("Dear \(players.guardianName), \n \(players.name) has been selected for the Dragon Team! The Dragons first pratice is on March 17, at 1pm! Please let us know if you have any questions or concerns. \n -League Staff \n")
-    }
-    print("\n")
-    print("******SHARKS*******")
-    for players in sharks {
-        print("Dear \(players.guardianName), \n \(players.name) has been selected for the Shark Team! The Sharks first pratice is on March 17, 3pm! Please let us know if you have any questions or concerns. \n -League Staff \n")
-    }
-    print("\n")
-    print("******RAPTORS*******")
-    for players in raptors {
-        print("Dear \(players.guardianName), \n \(players.name) has been selected for the Raptor Team! The Raptors first pratice is on March 18, at 1pm! Please let us know if you have any questions or concerns. \n -League Staff \n")
+        print("Dear \(players["name"]!), \n \(players["guardin"]!) has been selected for the Dragon Team! The Dragons first pratice is on March 17, at 1pm! Please let us know if you have any questions or concerns. \n -League Staff \n")
     }
     
-    return (dragons, sharks, raptors)
+    print("*****Team Sharks*****")
+    for players in sharks {
+        print("Dear \(players["name"]!), \n \(players["guardin"]!) has been selected for the Shark Team! The Sharks first pratice is on March 17, 3pm! Please let us know if you have any questions or concerns. \n -League Staff \n")
+    }
+    
+    print("*****Team Raptors*****")
+    for players in raptors {
+        print("Dear \(players["name"]!), \n \(players["guardin"]!) has been selected for the Raptor Team! The Raptors first pratice is on March 18, at 1pm! Please let us know if you have any questions or concerns. \n -League Staff \n")
+    }
+    
 }
 
+creatTeams()
+printTeamLetters()
 
-createTeams(soccerLeague)
